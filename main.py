e747944869325b21699c46b531a02c3b1af18ed1
PAULO <paulo.sousa@trt8.jus.br>
2024-05-15 21:56:19 -0300

    inserção das funções inserir_wumpus e inserir_ouro

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import random
 
 def criar_labirinto(n_linhas, n_colunas, valor):
-    matriz = []
-    for i in range(n_linhas):
-        linha = []
-        for j in range(n_colunas):
-            linha += [valor]
-        matriz += [valor]
-    return matriz
+    if n_linhas > 0 and n_colunas > 0:
+        matriz = []
+        for i in range(n_linhas):
+            linha = []
+            for j in range(n_colunas):
+                linha.append(valor)
+            matriz.append(linha)
+        return matriz
 
 def imprimir_labirinto(matriz):
-    linhas = len(matriz)
-    colunas = len(matriz[0])
-    for i in range(linhas):
-        for j in range(colunas):
-            print("%d" % matriz[i][j], end="")
-            if (j == colunas - 1):
-                print("%d" % matriz[i][j], end="")
+   for i in range(len(matriz)):
+       print("")
+       for j in range(len(matriz[0])):
+           print(matriz[i][j], end = " ")
 
-        print()
+
+def inserir_wumpus(matriz):
+    posicao_wumpus = random.randint(0, len(matriz)-1), random.randint(0, len(matriz[0])-1)
+    return posicao_wumpus
+
+def inserir_ouro(matriz, posicao_wumpus):
+    posicao_ouro =  random.randint(0, len(matriz)-1), random.randint(0, len(matriz[0])-1)
+    while posicao_ouro == posicao_wumpus:
+        posicao_ouro = random.randint(0, len(matriz)-1), random.randint(0, len(matriz[0])-1)
+    return posicao_ouro
 
 
 tm_matriz = int(input("Qual o tamanho da matriz?  "))
-matriz = criar_labirinto(tm_matriz, tm_matriz, "P")
+matriz = criar_labirinto(tm_matriz, tm_matriz, "?")
+posicao_wumpus = inserir_wumpus(matriz)
+posicao_ouro = inserir_ouro(matriz, posicao_wumpus)
+
+
+matriz[posicao_ouro[0]][posicao_ouro[1]] = "O"
+matriz[posicao_wumpus[0]][posicao_wumpus[1]] = 'W'
 imprimir_labirinto(matriz)
