b0e37910c7f9a85dda050863890805d7839a5884
PAULO <paulo.sousa@trt8.jus.br>
2024-05-19 22:24:22 -0300

    criação do labirinto completa

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ def imprimir_labirinto(matriz):
            print(matriz[i][j], end = " ")
 
 def imprimir_labirinto_jogador(matriz):
-    pass
+    for i in range(len(matriz)):
+        print("")
+        for j in range(len(matriz[0])):
+            print(matriz[i][j], end=" ")
 
 
 def inserir_wumpus(matriz):
@@ -31,6 +34,19 @@ def inserir_ouro(matriz, posicao_wumpus):
     while (posicao_ouro == posicao_wumpus) or (posicao_ouro == (0,0)):
         posicao_ouro = random.randint(0, len(matriz)-1), random.randint(0, len(matriz[0])-1)
     return posicao_ouro
+
+def adjacentes(x, y, matriz):
+    tamanho_matriz = len(matriz)
+    adj = []
+    if x > 0:
+        adj.append((x - 1, y))
+    if x < tamanho_matriz - 1:
+        adj.append((x + 1, y))
+    if y > 0:
+        adj.append((x, y - 1))
+    if y < tamanho_matriz - 1:
+        adj.append((x, y + 1))
+    return adj
 
 
 def inserir_pocos(matriz, posicao_wumpus, posicao_ouro):
@@ -83,18 +99,74 @@ def inserir_percepcao(matriz):
                         matriz[x][y] = "B"
     return matriz
 
-def movimento_jogador(matriz, movimento):
-    pass
+def movimento_jogador(campo_jogador, movimento):
+    posicao_jogador_x, posicao_jogador_y = buscar_jogador(campo_jogador)
+    tm_matriz = len(campo_jogador)
+    if posicao_jogador_x is None or posicao_jogador_y is None:
+        return
+
+    if movimento == "cima" and posicao_jogador_x > 0:
+        campo_jogador[posicao_jogador_x][posicao_jogador_y], campo_jogador[posicao_jogador_x - 1][posicao_jogador_y] = campo_jogador[posicao_jogador_x - 1][posicao_jogador_y], campo_jogador[posicao_jogador_x][posicao_jogador_y]
+    elif movimento == "baixo" and posicao_jogador_x < tm_matriz- 1:
+        campo_jogador[posicao_jogador_x][posicao_jogador_y], campo_jogador[posicao_jogador_x + 1][posicao_jogador_y] = campo_jogador[posicao_jogador_x + 1][posicao_jogador_y], campo_jogador[posicao_jogador_x][posicao_jogador_y]
+    elif movimento == "esquerda" and posicao_jogador_y > 0:
+        campo_jogador[posicao_jogador_x][posicao_jogador_y], campo_jogador[posicao_jogador_x][posicao_jogador_y - 1] = campo_jogador[posicao_jogador_x][posicao_jogador_y - 1], campo_jogador[posicao_jogador_x][posicao_jogador_y]
+    elif movimento == "direita" and posicao_jogador_y < tm_matriz - 1:
+        campo_jogador[posicao_jogador_x][posicao_jogador_y], campo_jogador[posicao_jogador_x][posicao_jogador_y + 1] = campo_jogador[posicao_jogador_x][posicao_jogador_y + 1], campo_jogador[posicao_jogador_x][posicao_jogador_y]
+    else:
+        print("Bateu na parede, faça outro movimento!")
+
+
+def buscar_jogador(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[0])):
+            if matriz[i][j] == "J":
+                return i, j
+    return None, None
+
+def verificar_percepcao(matriz, x, y):
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            if matriz[i][j] == "B" and x == i and y==j:
+                return ("O jogador está sentindo brisa")
+            elif matriz[i][j] == "F" and x == i and y==j:
+                return ("O jogador está sentindo fedor")
+            elif matriz[i][j] == "FB" and x == i and y==j:
+                return ("O jogador está sentindo fedor e brisa")
+            elif matriz[i][j] == "OB" and x == i and y==j:
+                return ("Parabéns o você pegou o ouro, mas está sentido brisa")
+            elif matriz[i][j] == "OF" and x == i and y==j:
+                return ("Parabéns você pegou o ouro, mas está sentido fedor")
+            elif matriz[i][j] == "P" and x == i and y==j:
+                return ("Você caiu no poço, fim de Jogo! :(")
+            elif matriz[i][j] == "W" and x == i and y==j:
+                return ("Você foi devorado pelo Wumpus, fim de Jogo! :(")
 
 
 def play(matriz):
     # inserir jogador
-    matriz[0][0] = "J"
+    campo_jogador = criar_labirinto(len(matriz), len(matriz), "?")
+    campo_jogador[0][0] = "J"
     while True:
-        print("Labirinto atual ...")
-        print(imprimir_labirinto_jogador(matriz))
+
+        print("\nLabirinto atual ... \n")
+        print(imprimir_labirinto(campo_jogador))
+        posicao_jogador_x, posicao_jogador_y = buscar_jogador(campo_jogador)
+        print(f"Posicao do jogador eh: {posicao_jogador_x} , {posicao_jogador_y} ")
+        jogada = verificar_percepcao(matriz, posicao_jogador_x, posicao_jogador_y)
+        print("\n")
+        if(jogada != None):
+            if(jogada == "Você caiu no poço, fim de Jogo! :("):
+                print(jogada)
+                break
+            elif(jogada == "Você foi devorado pelo Wumpus, fim de Jogo! :("):
+                print(jogada)
+                break
+            print(jogada, "\n")
         movimento = input("Para onde deseja mover (cima, baixo, esquerda, direita): ").lower()
-        movimento_jogador(matriz, movimento)
+        movimento_jogador(campo_jogador, movimento)
+
+
 
 
 
@@ -114,4 +186,4 @@ print()
 inserir_percepcao(matriz)
 print()
 imprimir_labirinto(matriz)
-
+play(matriz)
